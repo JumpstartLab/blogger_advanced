952d201d63d91cf8fb5e89124787c40de1578868
Jeff Casimir <jeff@casimircreative.com>
2012-01-10 20:39:12 -0500

    Reworked dashboard testing to better exercise statistics section

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,7 @@ class DashboardController < ApplicationController
     @articles = Article.for_dashboard
     @article_count = Article.count
     @article_word_count = Article.total_word_count
+    @most_popular_article = Article.most_popular
 
     @comments = Comment.for_dashboard
     @comment_count = Comment.count
--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -17,13 +17,15 @@ describe "front dashboard" do
     end
 
   	it "lists the five most recent articles" do
-      page.should have_content("Articles:")
+      page.should have_content("Most Recent Articles")
       all(".article").count.should eq(5)
     end
 
   	it "lists them in reverse chronological order" do
       titles = page.all('li.article').map(&:text)
-      titles.should eq(["Title 5", "Title 4", "Title 3", "Title 2", "Title 1"])
+      titles.each_with_index do |title, i|
+        title.should include("Title #{titles.size - i}")
+      end
     end
   end
 
@@ -37,51 +39,63 @@ describe "front dashboard" do
     end
 
     it "lists the five most recent comments" do
-      page.should have_content("Comments:")
+      page.should have_content("Most Recent Comments")
       all(".comment").count.should eq(5)
     end
 
     it "lists them in reverse order" do
-      bodies = page.all('li.comment').map(&:text)
-      bodies.should eq(["Body 5", "Body 4", "Body 3", "Body 2", "Body 1"])
+      listings = page.all('li.comment').map(&:text)
+      listings.each_with_index do |listing, i|
+        listing.should include("Body #{listings.size - i}")
+      end
     end
   end
 
-  context "statistics" do
+  context "#statistics" do
     it "lists the total number of articles" do
       number_of_articles = rand(10)
       number_of_articles.times { Fabricate(:article) }
-
       visit "/"
-      page.should have_content("Total articles: #{number_of_articles}")
+      page.find("#total_articles").should have_content("Total articles: #{number_of_articles}")
     end
 
     it "lists the total number of comments" do
       number_of_comments = rand(10)
       number_of_comments.times { Fabricate(:comment) }
-
       visit "/"
-      page.should have_content("Total comments: #{number_of_comments}")
+      page.find("#total_comments").should have_content("Total comments: #{number_of_comments}")
     end
-
 
     it "displays the total words of all articles" do
       Fabricate(:article, :body => "Four score and seven years...")
-
       visit "/"
-
-      page.should have_content("Total words: 5")
+      page.find("#total_article_words").should have_content("5")
     end
 
     it "displays the total words of all comments" do
       Fabricate(:comment, :body => "I think that...")
-
       visit "/"
-
-      page.should have_content("Total words: 3")
+      page.find("#total_comment_words").should have_content("3")
     end
-    it "displays the time since the most recent article"
-    it "displays the time since the most recent comment"
-    it "displays the most active (most comments) article"
+
+    it "displays the total words of all articles and comments" do      
+      article = Fabricate(:article, :body => "Four score and seven years...")
+      comment = Fabricate(:comment, :body => "I think that...", :article => article)
+      visit "/"
+      page.find("#total_words").should have_content(comment.word_count + article.word_count)
+    end
+
+    it "displays the most popular (most comments) article" do
+      articles = (0..3).collect{ Fabricate(:article) }
+      target = articles[rand(articles.size)]
+      Fabricate(:comment, :article => target)
+      visit "/"
+      page.find("#most_popular").should have_content(target.title)
+    end
+
+
+    # it "displays the time since the most recent article"
+    # it "displays the time since the most recent comment"
+
   end
 end
