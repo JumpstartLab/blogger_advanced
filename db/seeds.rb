a49dbdb09e5922e2f9838049a404ffec5e08744c
Steve Klabnik <steve@steveklabnik.com>
2013-04-26 11:11:00 -0700

    Add more articles to induce load

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Author.generate_samples(10)
-Tag.generate_samples(10)
-Article.generate_samples(10)
+Author.generate_samples(100)
+Tag.generate_samples(100)
+Article.generate_samples(1000)
