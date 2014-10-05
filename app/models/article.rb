cc00ad87e3e60a9c6931c88e512a44ec390ca7d9
Steve Kinney <hello@stevekinney.net>
2014-10-05 17:36:52 -0600

    #scoped is equivalent to #all in Rails 4

--- app/models/article.rb
+++ app/models/article.rb
@@ -41,7 +41,7 @@ class Article < ActiveRecord::Base
 
   def self.search_by_tag_name(tag_name)
     if tag_name.blank?
-      [Article.scoped, nil]
+      [Article.all, nil]
     else
       tag = Tag.find_by_name(tag_name)
       tag ? [tag.articles, tag] : [[], nil]
