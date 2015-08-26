c79d089cd25fe9a062189c94170ca0327aa9ab3b
worace <horace.d.williams@gmail.com>
2015-08-26 10:26:07 -0600

    basic selenium feature testing setup

--- spec/features/article_comments_spec.rb
+++ spec/features/article_comments_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
-describe "Article Comments", :type => :feature do
-  let(:article){ Fabricate(:article) }
+describe "Article Comments", :type => :feature, :js => true do
+
+  before(:each) do
+    visit new_article_path
+    fill_in "article_title", with: "My Article"
+    fill_in "article_body", with: "My Article Body"
+    click_link_or_button "Save"
+    click_link_or_button "My Article"
+  end
 
   it "posts a comment" do
-    visit article_path(article)
     fill_in "comment_author_name", :with => "Cowboy"
     fill_in "comment_body", :with => "Testing is too hard."
     click_link_or_button "post_comment"
