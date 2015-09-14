6c9f2ab94b7e52c68e8557172e7cd6c7e2bbd6ba
worace <horace.d.williams@gmail.com>
2015-09-13 18:32:07 -0600

    let's just go truncation on everything

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with(:truncation)
   end
 
